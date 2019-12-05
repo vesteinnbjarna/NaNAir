5c272bb712a077029e64632ee441cab9974b2fe4
vesteinnbjarna <vesteinn19@ru.is>
2019-12-05 16:25:58 +0000

    Apar ready

--- IO/IOAPI.py
+++ IO/IOAPI.py
@@ -8,8 +8,55 @@ from IO.permitIO import PermitIO
 class IOAPI ():
 
     def __init__(self):
-        pass
+        self.desIO = DestinationIO()
+        self.voyIO = VoyageIO()
+        self.plaIO = PlaneIO()
+        self.empIO = EmployeeIO()
+        self.rouIO = RouteIO()
+        self.perIO = PermitIO()
+        
 
-    pass
+    def loadDestinationFromFile (self):
+        return self.desIO.loadDestinationFromFile()
+    
+    def storeDestinationToFile(self):
+        return self.desIO.storeDestinationToFile()
 
+    def updateContactInfoInFile(self):
+        return self.desIO.updateContactInfoInFile()
 
+    def loadVoyagesFromFile (self):
+        return self.voyIO.loadVoyagesFromFile()
+    
+    def storeVoyageToFile(self):
+        return self.voyIO.storeVoyageToFile()
+
+    def updateVoyageInFile(self):
+        return self.voyIO.updateVoyageInFile()
+
+    def loadPlanesFromFile(self):
+        return self.plaIO.loadPlanesFromFiles()
+
+    def storePlaneToFile(self):
+        return self.plaIO.storePlaneToFile()
+
+    def loadEmployeesFromFile(self):
+        return self.empIO.loadEmployeesFromFile()
+
+    def storeEmployeeToFile(self):
+        return self.empIO.storeEmployeeToFile()
+
+    def updateEmployeeInFile(self):
+        return self.empIO.updateEmployeeInFile()
+
+    def loadRoutesFromFile(self):
+        return self.rouIO.loadRoutesFromFile()
+
+    def storeRouteToFile(self):
+        return self.rouIO.storeRouteToFile()
+
+    def loadPermitsFromFile(self):
+        return self.perIO.loadPermitsFromFile()
+    
+    def storePermitToFile(self):
+        return self.perIO.storePermitToFile()
--- LL/LLAPI.py
+++ LL/LLAPI.py
@@ -2,11 +2,52 @@ from LL.destinationLL import DestinationLL
 from LL.employeeLL import EmployeeLL
 from LL.planeLL import PlaneLL
 from LL.voyageLL import VoyageLL
+from IO.IOAPI import IOAPI
+
 class LLAPI():
     pass
 
     def __init__(self):
-        pass
+        self.ioAPI = IOAPI()
+        self.destLL = DestinationLL(self.ioAPI)
+        self.voyLL = VoyageLL(self.ioAPI)
+        self.plaLL = PlaneLL(self.ioAPI)
+        self.empLL = EmployeeLL(self.ioAPI)
+
+    def getDestinations(self):
+        return self.destLL.getDestination()
+    
+    def getDestinationInIceland(self):
+        return self.destLL.getDestinationInIceland()
+    
+    def updateContactInfo(self):
+        return self.destLL.updateContactInfo()
+
+    def createVoyage(self):
+        return self.voyLL.createVoyage()
+    
+    def getVoyages(self):
+        return self.voyLL.getVoyages()
+
+    def updateVoyage(self):
+        return self.voyLL.updateVoyage()
+    
+    def createPlane(self):
+        return self.plaLL.createPlane()
+
+    def getPlanes(self):
+        return self.plaLL.getPlanes()
+
+    def createEmployee(self):
+        return self.empLL.createEmployee()
+    
+    def getEmployee(self):
+        return self.empLL.getEmployee()
+
+    def updateEmployee(self):
+        return self.empLL.updateEmployee()
+
+    
 
 
 
